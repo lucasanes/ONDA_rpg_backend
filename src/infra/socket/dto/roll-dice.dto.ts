decb6d6c670fea8d0d559fcc2f9a70770d2d7d13
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-12-05 10:20:59 -0300

    feat: update RollDiceDto to include dice structure and additional properties; modify handleRollDice to emit new data

--- src/infra/socket/dto/roll-dice.dto.ts
+++ src/infra/socket/dto/roll-dice.dto.ts
@@ -1,8 +1,22 @@
+type Dice = {
+  bonus: string;
+  dice: string;
+  rollDices: {
+    faces: number;
+    quantity: number;
+    rolls: number[];
+    total: number;
+  }[];
+  total: number;
+};
+
 export interface RollDiceDto {
   characterId: number | null;
+  dice: Dice;
   isCritical: boolean;
   isD20: boolean;
   isDisaster: boolean;
+  name: string;
+  portrait: string | null;
   sessionId: number | null;
-  value: number;
 }
